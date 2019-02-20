dd287ec519b73e29e3a2eaf5f5c336f202d1f374
Zak Hassan <zak.hassan1010@gmail.com>
2019-02-20 10:59:39 -0500

    Fixing demo

--- start-driver-prom.sh
+++ start-driver-prom.sh
@@ -1,2 +1,5 @@
 
-spark-submit --driver-java-options "-javaagent:/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/metrics/agent-bond.jar=/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/conf/agent-d.properties"  --conf spark.driver.extraJavaOptions=-javaagent:/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/metrics/agent-bond.jar=/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/conf/agent-d.properties  --master   spark://10.230.8.242:7077  examples/src/main/python/pi.py
+# Update this env with the spark master url
+SPARK_MASTER_URL= # spark://10.230.8.242:7077
+
+spark-submit --driver-java-options "-javaagent:/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/metrics/agent-bond.jar=/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/conf/agent-d.properties"  --conf spark.driver.extraJavaOptions=-javaagent:/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/metrics/agent-bond.jar=/Users/zhassan/opt/spark-2.4.0-bin-hadoop2.7/conf/agent-d.properties  --master    $SPARK_MASTER_URL examples/src/main/python/pi.py
